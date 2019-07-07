b91ca53bd0efea14d0bb9d5ed470e7f7d2912299
AwwCookies <emma@gracejones.email>
2019-07-07 12:20:23 +0200

    Update aka.py

---
+++ Aka/aka.py
@@ -0,0 +1,18 @@
+import znc
+import os
+import urllib.request
+
+class aka(znc.Module):
+    module_types = [znc.CModInfo.NetworkModule]
+    description = "Tracks users, allowing tracing and history viewing of nicks, hosts, and channels"
+
+    def OnLoad(self, args, message):
+        
+        self.update()
+
+    def update(self):
+        new_version = urllib.request.urlopen("https://raw.githubusercontent.com/emagaliff/znc-nicktrace/master/aka.py")
+        with open(self.GetModPath(), 'w') as f:
+            f.write(new_version.read().decode('utf-8'))
+        
+        znc.CZNC().UpdateModule("aka")
