4e1388415873380433c29b3d2d0394dbfe82e8a1
AwwCookies <awwcookies@gmail.com>
2019-07-07 12:26:45 +0200

    Update aka.py

--- Aka/aka.py
+++ Aka/aka.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-import znc
-import os
-import urllib.request
-=======
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #   Authors: AwwCookies (Aww), MuffinMedic (Evan)                 #
 #   Last Update: Nov 17, 2015                                     #
@@ -35,25 +30,10 @@ DEFAULT_CONFIG = {
     "NOTIFY_ON_MODE": False, # 0/1
     "NOTIFY_ON_MODERATED": False # 0/1
 }
->>>>>>> parent of 1f551fe... Delete aka.py
 
 class aka(znc.Module):
     module_types = [znc.CModInfo.NetworkModule]
     description = "Tracks users, allowing tracing and history viewing of nicks, hosts, and channels"
-<<<<<<< HEAD
-
-    def OnLoad(self, args, message):
-        
-        self.update()
-
-    def update(self):
-        new_version = urllib.request.urlopen("https://raw.githubusercontent.com/emagaliff/znc-nicktrace/master/aka.py")
-        with open(self.GetModPath(), 'w') as f:
-            f.write(new_version.read().decode('utf-8'))
-        
-<<<<<<< HEAD
-        znc.CZNC().UpdateModule("aka")
-=======
     wiki_page = "aka"
 
     ''' PROCESS DATA '''
